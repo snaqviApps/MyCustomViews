c4e248ebd7748f95a1e701697b7a43fa21ffcd5a
snaqviApps <naqvi.sdsu@gmail.com>
2022-12-28 10:40:53 -0600

    added viewbinding

--- app/src/main/java/ghar/learn/mycustomviews/MainActivity.kt
+++ app/src/main/java/ghar/learn/mycustomviews/MainActivity.kt
@@ -6,19 +6,31 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import ghar.learn.mycustomviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBasedBinding : ActivityMainBinding
     private lateinit var customButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewBasedBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setContentView(viewBasedBinding.root)
 
-        customButton = findViewById<LinearLayout>(R.id.id_main_custmView).findViewById(R.id.cvButtonViewXML)
-        customButton.let {
+//        customButton = findViewById<LinearLayout>(R.id.id_main_custmView).findViewById(R.id.cvButtonViewXML)
+//        customButton.let {
+//            it.visibility = View.VISIBLE
+//            it.setOnClickListener {
+//                Log.i("bu", "Hi button")
+//            }
+//        }
+
+        viewBasedBinding.idMainCustmView.customMemberButton?.let {
             it.visibility = View.VISIBLE
             it.setOnClickListener {
-                Log.i("bu", "Hi button")
+                Log.i("bu", "Hi button using viewBinding")
             }
         }
 
