24996a3c33d52f52306b7a7ca5f0ab93390b78bb
snaqviApps <naqvi.sdsu@gmail.com>
2023-02-05 18:54:13 -0600

    passive fragment added programmatically

--- app/src/main/java/entry/MainActivity.kt
+++ app/src/main/java/entry/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
+import ghar.learn.mycustomviews.GithubUserProfileFragment
 import ghar.learn.mycustomviews.R
 import ghar.learn.mycustomviews.api.BackEndRepository
 import ghar.learn.mycustomviews.databinding.ActivityMainBinding
@@ -17,21 +19,28 @@ import ghar.learn.mycustomviews.views.BasicRetrofitCallViewModelFactory
 class MainActivity : AppCompatActivity() {
 
     private val _TAG: String by lazy { localClassName }
-    private lateinit var repository : BackEndRepository         // for later considerations
-    private lateinit var viewBasedBinding : ActivityMainBinding
-    private lateinit var viewBasedBindingCustomView : CustomviewfirstBinding
-    private lateinit var viewModel : BasicRetrofitCallViewModel
+    private lateinit var repository: BackEndRepository         // for later considerations
+    private lateinit var viewBasedBinding: ActivityMainBinding
+    private lateinit var viewBasedBindingCustomView: CustomviewfirstBinding
+    private lateinit var viewModel: BasicRetrofitCallViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container_view_main, GithubUserProfileFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
         viewBasedBinding = ActivityMainBinding.inflate(layoutInflater)
         viewBasedBindingCustomView = CustomviewfirstBinding.inflate(layoutInflater)
 
-//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)     // no data-binding approach
         setContentView(viewBasedBinding.root)
 
+
 //        viewModel = ViewModelProvider(this)[BasicRetrofitCallViewModel::class.java]       // no-factory approach
-        viewModel = ViewModelProvider(this, BasicRetrofitCallViewModelFactory(repository = BackEndRepository()))[BasicRetrofitCallViewModel::class.java]
+        viewModel = ViewModelProvider(this,
+            BasicRetrofitCallViewModelFactory(repository = BackEndRepository()))[BasicRetrofitCallViewModel::class.java]
         viewBasedBindingCustomView.customXMLViewModel = viewModel
 //        viewBasedBinding = ViewTreeLifecycleOwner
 
@@ -42,29 +51,27 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Hi from custom-button", Toast.LENGTH_SHORT).show()
                 viewModel.uiDataProvider.observe(this) { buttonGithubAccess: GithubPojo? ->
                     buttonGithubAccess?.let {
-                        Toast.makeText(this, "gitHub call response list size: ${buttonGithubAccess}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(
+                            this,
+                            "gitHub call response list size: ${buttonGithubAccess}",
+                            Toast.LENGTH_LONG
+                        ).show()
                         Log.i("main", "Hi button using githubResponse : ${buttonGithubAccess}")
-                    } ?:run {
-                        Toast.makeText(this, "gitHub call response list size is zero: ${buttonGithubAccess}", Toast.LENGTH_SHORT).show()
+                    } ?: run {
+                        Toast.makeText(
+                            this,
+                            "gitHub call response list size is zero: ${buttonGithubAccess}",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         Log.i(_TAG, "Hi button using githubResponse : ${buttonGithubAccess ?: ""}")
                     }
                 }
             }
         }
-//        displayGithubResponseInToast()
+//        displayFragment()
     }
 
     private fun displayGithubResponseInToast() {
-//        viewModel.uiDataProvider.observe(this, Observer{
-        this.viewModel.uiDataProvider.observe(this) {
-            it?.let {
-                Toast.makeText(this, "gitHub call response list size: ${it}", Toast.LENGTH_SHORT).show()
-                Log.i(_TAG, "Hi button using githubResponse : $it")
 
-            } ?: run {
-                Toast.makeText(this, "gitHub call response came with No results", Toast.LENGTH_SHORT).show()
-            }
-
-        }
     }
 }
